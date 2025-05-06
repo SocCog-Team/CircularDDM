1bd8cae03dc5c2037354ad3f7146971c749e16d9
Igor Kagan <igor.b.kagan@gmail.com>
2025-05-06 13:50:03 +0200

    Update leakyIntegrator.m

--- leaky_integrator/leakyIntegrator.m
+++ leaky_integrator/leakyIntegrator.m
@@ -8,8 +8,17 @@ nd = round(d / dt);  % number of time steps for the delay
 s_delayed = [zeros(1, nd), s];
 s_delayed = s_delayed(1:nt);  % ensure same length as t
 
+% with loop
 y = zeros(1, nt);
 for i = 1:(nt - 1)
     dy = s_delayed(i) - y(i)/k;
     y(i+1) = y(i) + dy * dt;
 end
+
+% without loop
+% Filter coefficients to match the loop
+a = [1, -(1 - dt/k)];  % denominator: y[n] - (1 - dt/k)*y[n-1]
+b = dt;                % numerator: dt * s[n]
+
+% Filter signal
+y = filter(b, a, s_delayed);
